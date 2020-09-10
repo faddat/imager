e1355a8dddec23c2d7d03a3886ced9dd338851c0
Jacob Gadikian <jacobgadikian@gmail.com>
2020-09-10 15:04:32 +0700

    update action

---
+++ installenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+
+# INSTALL DEPENDENCIES
+DEBIAN_FRONTEND="noninteractive" apt install -y ca-certificates git unzip wget qemu-user-static build-essential qemu-user-static ca-certificates dosfstools gdisk kpartx parted libarchive-tools sudo xz-utils psmisc
+git clone https://github.com/hashicorp/packer --branch v1.5.5
+cd packer
+go mod vendor
+go get .
+go install
+cd ..
+
+# INSTALL PACKER-BUILDER-ARM
+git clone https://github.com//mkaczanowski/packer-builder-arm/
+cd packer-builder-arm
+go mod download
+go install
